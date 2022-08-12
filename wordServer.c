f27cd98e906fa3ef1d5d8f8d8741bde30bad1ab7
usmaan6 <102833275+usmaan6@users.noreply.github.com>
2022-08-11 21:59:02 -0500

    Update wordServer.c

--- wordServer.c
+++ wordServer.c
@@ -2,13 +2,13 @@
 //	Compile with:
 //	$ gcc wordServer.c -o wordServer -lpthread
 
-//---		Header file inclusion					---//
+
 
 #include	"wordClientServer.h"
 #include	<pthread.h>	// For pthread_create()
 
 
-//---		Definition of constants:				---//
+
 
 #define		STD_OKAY_MSG		"Okay"
 
@@ -25,7 +25,7 @@
 const int	ERROR_FD		= -1;
 
 
-//---		Definition of functions:				---//
+
 
 void handleClient (void* vPtr)
 {
@@ -35,7 +35,7 @@ void handleClient (void* vPtr)
 
   free(numArra);
 
-  //  II.B.  Read command:
+ 
   char	buffer[BUFFER_LEN];
   char	command;
   int	fileNum;
@@ -50,7 +50,7 @@ void handleClient (void* vPtr)
     printf("Thread %d received: %s\n",fileNum,buffer);
     sscanf(buffer,"%c %d \"%[^\"]\"",&command,&fileNum,text);
 
-    // YOUR CODE HERE
+   
 
     if (command == DIR_CMD_CHAR)
       {
@@ -69,12 +69,11 @@ void handleClient (void* vPtr)
       {
 	char filename[BUFFER_LEN];
 	snprintf(filename,BUFFER_LEN,"%d.txt",fileNum);
-	//	printf("writeCmd, before sizing text: text = %s \n", &text);
-	//        int sourceFd = open(buffer,O_RDONLY);
+	
 	int destFd = open(filename,O_WRONLY|O_CREAT|O_TRUNC, 0660);
 	int numBytes;
 	size_t textLen = strlen(text);
-	//	printf("clientFd = %d, fileNum = %d, text = %s, textLen = %d \n",clientFd,fileNum,text, textLen);
+	
 
 	      	if (destFd > 0)
 	  	  {
@@ -170,14 +169,11 @@ void handleClient (void* vPtr)
   }  
 }  
 
-//  PURPOSE:  To run the server by 'accept()'-ing client requests from
-//	'listenFd' and doing them.
+
 void		doServer	(int		listenFd
 				)
 {
-  //  I.  Application validity check:
-
-  //  II.  Server clients:
+ 
   pthread_t		threadId;
   pthread_attr_t	threadAttr;
   int			threadCount	= 0;
@@ -206,15 +202,12 @@ void		doServer	(int		listenFd
 }
 
 
-//  PURPOSE:  To decide a port number, either from the command line arguments
-//	'argc' and 'argv[]', or by asking the user.  Returns port number.
+
 int		getPortNum	(int	argc,
 				 char*	argv[]
 				)
 {
-  //  I.  Application validity check:
-
-  //  II.  Get listening socket:
+  
   int	portNum;
 
   if  (argc >= 2)
@@ -228,24 +221,19 @@ int		getPortNum	(int	argc,
     portNum	= strtol(buffer,NULL,0);
   }
 
-  //  III.  Finished:  
+ 
   return(portNum);
 }
 
 
-//  PURPOSE:  To attempt to create and return a file-descriptor for listening
-//	to the OS telling this server when a client process has connect()-ed
-//	to 'port'.  Returns that file-descriptor, or 'ERROR_FD' on failure.
+
 int		getServerFileDescriptor
 				(int		port
 				)
 {
-  //  I.  Application validity check:
-
-  //  II.  Attempt to get socket file descriptor and bind it to 'port':
-  //  II.A.  Create a socket
-  int socketDescriptor = socket(AF_INET, // AF_INET domain
-			        SOCK_STREAM, // Reliable TCP
+ 
+  int socketDescriptor = socket(AF_INET, 
+			        SOCK_STREAM, 
 			        0);
 
   if  (socketDescriptor < 0)
@@ -254,24 +242,23 @@ int		getServerFileDescriptor
     return(ERROR_FD);
   }
 
-  //  II.B.  Attempt to bind 'socketDescriptor' to 'port':
-  //  II.B.1.  We'll fill in this datastruct
+ 
   struct sockaddr_in socketInfo;
 
-  //  II.B.2.  Fill socketInfo with 0's
+ 
   memset(&socketInfo,'\0',sizeof(socketInfo));
 
-  //  II.B.3.  Use TCP/IP:
+  
   socketInfo.sin_family = AF_INET;
 
-  //  II.B.4.  Tell port in network endian with htons()
+  
   socketInfo.sin_port = htons(port);
 
-  //  II.B.5.  Allow machine to connect to this service
+  
   socketInfo.sin_addr.s_addr = INADDR_ANY;
 
-  //  II.B.6.  Try to bind socket with port and other specifications
-  int status = bind(socketDescriptor, // from socket()
+  
+  int status = bind(socketDescriptor, 
 		    (struct sockaddr*)&socketInfo,
 		    sizeof(socketInfo)
 		   );
@@ -282,10 +269,10 @@ int		getServerFileDescriptor
     return(ERROR_FD);
   }
 
-  //  II.B.6.  Set OS queue length:
+ 
   listen(socketDescriptor,5);
 
-  //  III.  Finished:
+  
   return(socketDescriptor);
 }
 
@@ -294,9 +281,9 @@ int		main		(int	argc,
 				 char*	argv[]
 				)
 {
-  //  I.  Application validity check:
+ 
 
-  //  II.  Do server:
+  
   int 	      port	= getPortNum(argc,argv);
   int	      listenFd	= getServerFileDescriptor(port);
   int	      status	= EXIT_FAILURE;
@@ -308,6 +295,6 @@ int		main		(int	argc,
     status	= EXIT_SUCCESS;
   }
 
-  //  III.  Finished:
+  
   return(status);
 }
